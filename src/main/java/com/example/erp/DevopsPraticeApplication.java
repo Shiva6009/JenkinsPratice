79d95e9ca9cf4e16bf5273e7a8d88a89eebcdff7
Siva Velusamy <siva.v@NSOFTINDIA.INT>
2022-11-09 15:46:19 +0530

    First Commit

---
+++ src/main/java/com/example/erp/DevopsPraticeApplication.java
@@ -0,0 +1,14 @@
+package com.example.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPraticeApplication.class, args);
+		System.out.println(" Server is Running");
+	}
+
+}
